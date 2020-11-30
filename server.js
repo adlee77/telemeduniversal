1aff297dc68f7bf1795ec8c740095f7dfa64f4eb
Andrew Lee <andrewimg@gmail.com>
2020-11-30 12:16:16 -0800

    https

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var exphbs = require('express-handlebars')
+var enforce = require('express-sslify');
+
 
 var db = require('./models')
 
 var app = express()
 var PORT = process.env.PORT || 3000
-
+app.use(enforce.HTTPS());
 const bodyParser = require('body-parser')
 const path = require('path')
 
